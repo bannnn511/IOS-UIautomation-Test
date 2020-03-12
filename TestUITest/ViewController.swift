176487c57d90954fe57ad038d8ca5d590944b212
khai <khai@user.sg>
2020-03-12 09:52:48 +0700

    Initial Commit

---
+++ TestUITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestUITest
+//
+//  Created by Mac on 3/12/20.
+//  Copyright © 2020 Khai Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
