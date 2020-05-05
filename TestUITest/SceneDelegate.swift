2ad318502342645e06fbd2ef6b85242ce5d4041b
An <habinhan1999@gmail.com>
2020-05-05 15:37:52 +0700

    commit b4 merge

--- TestUITest/SceneDelegate.swift
+++ TestUITest/SceneDelegate.swift
@@ -23,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         self.window = UIWindow(windowScene: windowScene)
         //self.window =  UIWindow(frame: UIScreen.main.bounds)
-        let rootVC = UINavigationController(rootViewController: ViewController())
+        let rootVC = UINavigationController(rootViewController: ScheduleViewController())
         self.window?.rootViewController = rootVC
         self.window?.makeKeyAndVisible()
     }
